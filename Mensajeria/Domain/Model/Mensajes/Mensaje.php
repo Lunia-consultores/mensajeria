4e4b1368243aba9ce3fb48a68309632ba1a00c66
Jaime <jmulero@lunia.es>
2022-06-13 10:00:15 +0200

    TILUNIA-772 Publicar la librería en github

--- Mensajeria/Domain/Model/Mensajes/Mensaje.php
+++ Mensajeria/Domain/Model/Mensajes/Mensaje.php
@@ -87,6 +87,12 @@ class Mensaje
         return $this->respuesta;
     }
 
+    public function compararContenido(Mensaje $otroMensaje): bool {
+        return ($this->payload === $otroMensaje->payload) &&
+            ($this->routingKey === $otroMensaje->routingKey) &&
+            ($this->esSincrono() === $otroMensaje->esSincrono()) &&
+            ($this->esRespuesta() === $otroMensaje->esRespuesta());
+    }
 
 
 }
